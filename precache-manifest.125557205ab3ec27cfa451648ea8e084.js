53780bb3bd528624e833b566fd6d6cc9ba560876
= <gustacabj1022@gmail.com>
2020-09-25 20:24:12 -0300

    Updates

---
+++ precache-manifest.125557205ab3ec27cfa451648ea8e084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d78cc9b5d13f2dcac87349af5ceabd4",
+    "url": "/delivery-eat/index.html"
+  },
+  {
+    "revision": "077f41f8a9a4ce77ee2a",
+    "url": "/delivery-eat/static/js/2.0ef793b4.chunk.js"
+  },
+  {
+    "revision": "b0382a1d868abef989ec0625e2034248",
+    "url": "/delivery-eat/static/js/2.0ef793b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c2f5db1debd28018d6",
+    "url": "/delivery-eat/static/js/main.8d4b2974.chunk.js"
+  },
+  {
+    "revision": "a3bef0dd596f6d765300",
+    "url": "/delivery-eat/static/js/runtime-main.11844eee.js"
+  },
+  {
+    "revision": "b92791c378d1ddd892815fbf033da4b3",
+    "url": "/delivery-eat/static/media/listo2.b92791c3.png"
+  }
+]);
